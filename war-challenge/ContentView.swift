66f3046c3d81ff0bfaccbe3a9d680e90d445ef5f
Shashank Kumar <shashank.cs18@bmsce.ac.in>
2021-09-04 15:37:22 +0530

    Update ContentView.swift

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    // @State is a property state wrapper which can be changed
+    @State var playerCard="card5"
+    @State var cpuCard="card9"
+    @State var playerScore=0
+    @State var cpuScore=0
+    
+    
+    
     var body: some View {
         ZStack{
             Image("background").ignoresSafeArea()
@@ -18,29 +26,43 @@ struct ContentView: View {
                 Spacer()
             HStack(){
                 Spacer()
-                Image("card3")
+                Image(playerCard)
                 Spacer()
-                Image("card4")
+                Image(cpuCard)
                 Spacer()
             }
                 Spacer()
-               Button(action: {
-                    print("Button pressed")
+                
+                Button(action: {
+                    //Generate a random number between 2 and 14
+                    let playerRand=Int.random(in: 2...14)
+                    let cpuRand=Int.random(in: 2...14)
+                    //Update the card
+                    playerCard="card" + String(playerRand)
+                    cpuCard="card" + String(cpuRand)
+                    //Update the score
+                    //playerScore+=1
+                   // cpuScore+=1
+                    
                 }, label: {
                     Image("dealbutton")
                         .padding(.all)
                 })
+
+                
+                
+                
                 Spacer()
             HStack(){
                 VStack{
                     Text("PLAYER") .foregroundColor(Color.white).font(.title2)
-                    Text("0") .foregroundColor(Color.white).font(.title2)
+                    Text(String(playerScore)) .foregroundColor(Color.white).font(.title2)
                     
                 }
                 .padding(.all)
                 VStack{
                     Text("CPU").fontWeight(.regular).foregroundColor(Color.white).font(.title2).lineLimit(0)
-                    Text("0").fontWeight(.medium) .foregroundColor(Color.white).font(.title2)
+                    Text(String(cpuScore)).fontWeight(.medium) .foregroundColor(Color.white).font(.title2)
                 }
                 .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                 }
@@ -56,3 +78,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
